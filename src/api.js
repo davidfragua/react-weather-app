5606cfd221e20b0c407204f229872a5872823f15
David Fragua <davidfragua@gmail.com>
2022-11-28 13:55:12 +0100

    initial structure and search feature

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3a5d848ddbmsh513a75a6e62eebcp150e94jsnc383cac66cbc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
