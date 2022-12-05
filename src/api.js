c5fdf824d9f26484e7fb75a93f67b5c3a8e8b28d
David Fragua <davidfragua@gmail.com>
2022-12-05 16:24:42 +0100

    weather app done

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-        'X-RapidAPI-Key': process.env.RAPID_API_KEY, 
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-		
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -13,12 +13,12 @@ const Search = ({ onSearchChange }) => {
       .then((response) => response.json())
       .then((response) => {
         return {
-            options: response.data.map((city) =>{
-                return {
-                    value: `${city.latitude} ${city.longitude}`,
-                    label: `${city.name} ${city.countryCode}`,
-                };
-            }),
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name} ${city.countryCode}`,
+            };
+          }),
         };
       })
       .catch((err) => console.error(err));
